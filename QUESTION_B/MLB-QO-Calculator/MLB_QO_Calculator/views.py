57a45749106c59f577611887884c8b1adf1d58b4
micah-kim <kimerr@bc.edu>
2022-08-07 23:08:50 -0400

    adding design

--- QUESTION_B/MLB-QO-Calculator/MLB_QO_Calculator/views.py
+++ QUESTION_B/MLB-QO-Calculator/MLB_QO_Calculator/views.py
@@ -5,18 +5,28 @@ from bs4 import BeautifulSoup
 
 # render index.html when the button is not clicked
 def button(request):
-    return render(request, 'index.html')
+    url = "https://questionnaire-148920.appspot.com/swe/data.html"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    
+    data = soup.find('table', class_ = "table")
+    return render(request, 'index.html', {'tableData' : data})
 
+# re-render index.html with output
 def output(request):
+    # extract url of site to be parsed
     url = "https://questionnaire-148920.appspot.com/swe/data.html"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     
+    # search site html for table
     MLB_table = soup.find('table', class_ = "table")
     arr = []
     total = 0
     count = 0
     data = 0
+    
+    # iterate through individual players and store salary in "arr"
     for player in MLB_table.find_all("tbody"):
         rows = player.find_all("tr")
         for row in rows:
@@ -25,8 +35,10 @@ def output(request):
             if salary != '' and salary.isdigit():
                 arr.append(int(salary))
     
+    # sort array in reverse order by salary amount
     arr = sorted(arr, reverse=True)
     
+    # iterate array again get highest 125 salaries
     for a in arr:
         total+=a
         count+=1
@@ -35,6 +47,7 @@ def output(request):
             data = total // 125
             data = "{:,}".format(data)
             data = '$' + data
-            return render(request, 'index.html', {'data' : data})
+            return render(request, 'index.html', {'data' : data, 'img' : "https://imgur.com/LmekkPz"})
 
+    # will render 0 as the data if arr is empty
     return render(request, 'index.html', {'data' : data})
